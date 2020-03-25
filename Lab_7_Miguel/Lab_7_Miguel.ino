ba2326375bd72216fb3ef5cb0a0974286d31d9e0
gar17560 <45210913+gar17560@users.noreply.github.com>
2020-03-25 15:43:25 -0600

    Parte 1.1
    Se crearon los archivos y se configuro para mostrar los archivos

---
+++ Lab_7_Miguel/Lab_7_Miguel.ino
@@ -0,0 +1,67 @@
+/*
+  Laboratorio 7
+
+  Miguel García  
+  17560
+  Electrónica digital 2
+  Sección: 20
+
+*/
+
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
+}

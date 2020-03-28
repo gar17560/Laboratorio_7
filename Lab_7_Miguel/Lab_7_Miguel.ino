5d72fc0d8e975ace9481f128ef6b223f17b1625b
gar17560 <45210913+gar17560@users.noreply.github.com>
2020-03-27 20:31:35 -0600

    LAB COMPLETO
    Se agregó la parte 2.

--- Lab_7_Miguel/Lab_7_Miguel.ino
+++ Lab_7_Miguel/Lab_7_Miguel.ino
@@ -12,15 +12,21 @@
 #include <SD.h>
 #include <SoftwareSerial.h>
 
+
+// Variables
 File root;
+File myFile;
 char a;
-int cont = 1;
+char b;
+char c;
+char d;
+int cont = 2;
+
+
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  while (!Serial);
 
   Serial.println("INICIANDO SD DE MIGUEL");
 
@@ -34,23 +40,79 @@ void setup() {
 
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  while(!Serial.available()){
-  }
+  Serial.println("Escriba el número de archivo que desea abrir:");
+  c = Serial.read();
+  while(!Serial.available());
   
   a = Serial.read();
   Serial.print("Su opción es:");
   Serial.print("\t");
   Serial.println(a);
-  delay(1000);
+  delay(500);
 
+  leer_arch(a);
+  c = Serial.read();
+  Serial.println("Desea volver a mostrar la lista de archivos:");
+  Serial.println("1 = Sí   1> = No");
+  
+  while(!Serial.available());
+  b = Serial.read();
+  if(b == 49){
+    cont = 2;
+    mostrar_arch();
+    c = Serial.read();
+    }
+  if(b>49){
+    Serial.println("No"); 
+    c = Serial.read();
+    while(!Serial.available());
+  }
+  
 
 }
 
 
 void leer_arch(char a){
+  if (a == 52){
+    myFile = SD.open("spider~1.txt");
+    Serial.println("");
+    Serial.println("spider~1.txt:");
+    Serial.println("");
+  }
+  if (a == 53){
+    myFile = SD.open("corazon.txt");
+    Serial.println("");
+    Serial.println("corazon.txt:");
+    Serial.println("");
+  }
+
+  if (a == 54){
+    myFile = SD.open("deadpool.txt");
+    Serial.println("");
+    Serial.println("deadpool.txt:");
+    Serial.println("");
+  }
+
+  if (a == 55){
+    myFile = SD.open("hongo.txt");
+    Serial.println("");
+    Serial.println("hongo.txt:");
+    Serial.println("");
+  }
   
+  if (myFile) {
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    Serial.println("");
+    // close the file:
+    myFile.close();
+  } 
+  else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
 
 
